2ffe983cff6188584e395881435f20bfe99430b1
rohitjoshi21 <joshi.rk11166@gmail.com>
2025-03-01 10:58:22 +0545

    console

--- layers.py
+++ layers.py
@@ -49,8 +49,6 @@ class TacotronSTFT(torch.nn.Module):
         self.stft_fn = STFT(filter_length, hop_length, win_length)
         mel_basisnp = librosa.filters.mel(
             sr=sampling_rate, n_fft=filter_length, n_mels=n_mel_channels, fmin=mel_fmin, fmax=mel_fmax)
-        print(type(mel_basisnp))  # Should print <class 'numpy.ndarray'>
-        print(mel_basisnp.shape) 
         self.register_buffer('mel_basis', torch.tensor(mel_basisnp, dtype=torch.float32))
 
     def spectral_normalize(self, magnitudes):
